d1e61ecb81f139407fb2e09a3cffde90fb217b9a
abdullahnazzal <a.nazzal1995@gmail.com>
2021-06-20 16:56:50 +0300

    added file

---
+++ js/app.js
@@ -0,0 +1,17 @@
+let userName;
+let userAge;
+let favCategory;
+let flag;
+
+alert('Welcome to my fav movies');
+flag = prompt("Is this the first time you enter on the site ");
+userName = prompt("Can you tell us your name:");
+userAge = prompt("Can you tell us your age:");
+userfavCategory = prompt("Can you tell us your fav category:");
+
+alert("Welcome "+userName+" to my fav movies ");
+alert("you choice "+userfavCategory+" Category, So We suggestion to wacth Edge of Tomorrow"+
+" and this accordance with your Age "+userAge+ " ");
+
+
+
